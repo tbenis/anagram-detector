fafea3c102b81a54d4d483ae235bc301dd18d846
Benis Tambe <will.jenifer@yahoo.com>
2021-01-25 20:03:16 -0500

    commit tree e218b63dd9e870688f77d623d0147dc9776fdcf1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :match
+
+    def initialize(word)
+        @instance_word = word
+    end
+
+    def match(array)
+        sorted = @instance_word.split("").sort().join()
+        arr = [] 
+        array.each do |x|
+            if x.split("").sort().join() == sorted
+                arr << x
+            end
+        end
+        arr
+    end
+end
